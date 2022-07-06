c563e189b79c739c9314a3ecb5a261b9733dd25e
phamhuu2701 <phamhuu2701@gmail.com>
2022-07-06 23:21:11 +0700

    update code

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: "backend",
+      script: "npm",
+      args: "run serve",
+      env_development: {
+        NODE_ENV: "development",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
