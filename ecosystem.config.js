733e6e33f12d31fb9076a5189fa6ed8ade78fefa
Huu Arena <huupham.arena@gmail.com>
2022-07-06 18:08:20 +0700

    add pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,59 @@
+require('dotenv').config()
+
+const {
+  PORT,
+  POSTGRES_USER,
+  POSTGRES_PWD,
+  POSTGRES_HOST,
+  POSTGRES_PORT,
+  POSTGRES_DB,
+  JWT_EXPIRATION,
+  JWT_SECRET,
+  CLOUDINARY_FOLDER,
+  CLOUDINARY_NAME,
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET_KEY,
+} = process.env
+
+module.exports = {
+  apps: [
+    {
+      name: 'arena-fullstack-app',
+
+      script: 'npm',
+      args: 'run serve',
+
+      env_development: {
+        NODE_ENV: 'development',
+        PORT,
+        POSTGRES_USER,
+        POSTGRES_PWD,
+        POSTGRES_HOST,
+        POSTGRES_PORT,
+        POSTGRES_DB,
+        JWT_EXPIRATION,
+        JWT_SECRET,
+        CLOUDINARY_FOLDER,
+        CLOUDINARY_NAME,
+        CLOUDINARY_API_KEY,
+        CLOUDINARY_SECRET_KEY,
+      },
+
+      env_production: {
+        NODE_ENV: 'production',
+        PORT,
+        POSTGRES_USER,
+        POSTGRES_PWD,
+        POSTGRES_HOST,
+        POSTGRES_PORT,
+        POSTGRES_DB,
+        JWT_EXPIRATION,
+        JWT_SECRET,
+        CLOUDINARY_FOLDER,
+        CLOUDINARY_NAME,
+        CLOUDINARY_API_KEY,
+        CLOUDINARY_SECRET_KEY,
+      },
+    },
+  ],
+}
